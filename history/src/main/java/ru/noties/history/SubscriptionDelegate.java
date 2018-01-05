828c0c6222631c428ef50b6d663a90532d72268b
Dimitry Ivanov <mail@dimitryivanov.ru>
2018-01-05 19:08:57 +0300

    Added SubscriptionDelegate (implements both Subscription and Visitor)

---
+++ history/src/main/java/ru/noties/history/SubscriptionDelegate.java
@@ -0,0 +1,32 @@
+package ru.noties.history;
+
+import android.support.annotation.NonNull;
+
+public class SubscriptionDelegate implements Subscription, Subscription.Visitor {
+
+    private Subscription subscription;
+
+    @Override
+    public void visit(@NonNull Subscription subscription) {
+        this.subscription = subscription;
+    }
+
+    @Override
+    public void unsubscribe() {
+        if (subscription != null) {
+            subscription.unsubscribe();
+            subscription = null;
+        }
+    }
+
+    @NonNull
+    @Override
+    public Subscription accept(@NonNull Visitor visitor) {
+        visitor.visit(this);
+        return this;
+    }
+
+    public boolean hasSubscription() {
+        return subscription != null;
+    }
+}
